272c079576cf74cba3510506ef16f480d639cdb8
jodrack <drack.sir01@gmail.com>
2022-11-26 08:56:35 +0200

    Remove url variable since it's not used there

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -7,7 +7,7 @@ export default function Nav() {
   return (
     <nav className='relative'>
       <div className='flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide'>
-        {Object.entries(requests).map(([key, { title, url }]) => (
+        {Object.entries(requests).map(([key, { title }]) => (
             <h2
               key={key}
               onClick={() => router.push(`/?genre=${key}`)}
